d585cb6349986bd6728c186472f08ed8b8bab84b
梁皓鈞 <a0985065381@gmail.com>
2022-12-04 14:58:56 +0800

    電影清單功能

---
+++ index.js
@@ -0,0 +1,158 @@
+const BASE_URL = 'https://movie-list.alphacamp.io'
+const INDEX_URL = BASE_URL + '/api/v1/movies/'
+const POSTER_URL = BASE_URL + '/posters/'
+const MOVIES_PER_PAGE = 12
+
+const movies = []
+let filteredMovies = []
+
+const dataPanle = document.querySelector('#data-panel')
+const searhForm = document.querySelector("#search-form")
+const searchInput = document.querySelector("#search-input")
+const paginator = document.querySelector("#paginator")
+
+//將電影資料帶入頁面
+function renderMovieList(data) {
+  let rawHTML = ''
+  data.forEach((item) => {
+    rawHTML += `
+    
+       <div class="col-sm-3">
+        <div class="mb-2">
+          <div class="card">
+            <img
+              src="${POSTER_URL + item.image}"
+              class="card-img-top" alt="Movie Poster" />
+            <div class="card-body">
+              <h5 class="card-title">${item.title}</h5>
+            </div>
+            <div class="card-footer">
+              <button class="btn btn-primary btn-show-movie" data-bs-toggle="modal" data-bs-target="#movie-modal" data-id="${item.id}">
+                More
+              </button>
+              <button class="btn btn-info btn-add-favorite" data-id="${item.id}">+</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    `
+  })
+
+  dataPanle.innerHTML = rawHTML
+}
+
+//計算總共頁數
+function renderPaginator(amount) {
+  const numberOfPages = Math.ceil(amount / MOVIES_PER_PAGE)
+  let rawHTML = ''
+  for (let page = 1; page <= numberOfPages; page++) {
+    rawHTML += `
+    <li class="page-item"><a class="page-link" href="#" data-page="${page}">${page}</a></li>
+    `
+  }
+  paginator.innerHTML = rawHTML
+}
+
+//監聽器頁面
+paginator.addEventListener('click', function onPaginatorClicked(event) {
+  //如果被點擊的不是 a 標籤，結束
+  if (event.target.tagName !== 'A') return
+  const page = Number(event.target.dataset.page)
+  renderMovieList(getMoviesByPage(page))
+})
+
+//負責切頁面的資料，第一筆0 ~ 11，slice() 結尾的 index 並不會包含在新陣列中
+function getMoviesByPage(page) {
+  //如果搜尋清單有東西，就取搜尋清單 filteredMovies，否則就還是取總清單 movies
+  const data = filteredMovies.length ? filteredMovies : movies
+  const startIndex = (page - 1) * MOVIES_PER_PAGE
+
+  return data.slice(startIndex, startIndex + MOVIES_PER_PAGE)
+}
+
+
+
+//將電影詳細資料帶入分頁
+function showMovieModel(id) {
+  const modalTitle = document.querySelector('#movie-modal-title')
+  const modalImage = document.querySelector('#movie-modal-image')
+  const modalDate = document.querySelector('#movie-modal-date')
+  const modalDescription = document.querySelector('#movie-modal-description')
+
+
+  axios.get(INDEX_URL + id).then((response) => {
+    const data = response.data.results
+    modalTitle.innerText = data.title
+    modalDate.innerText = 'Release date: ' + data.release_date
+    modalDescription.innerText = data.description
+    modalImage.innerHTML = `<img src="${POSTER_URL + data.image
+      }" alt="movie-poster" class="img-fluid">`
+
+  })
+}
+
+
+
+
+//將使用者點擊到的那一部電影送進 local storage 儲存起來
+//JSON.parse將資料轉為JS物件
+function addToFavorite(id) {
+  const list = JSON.parse(localStorage.getItem('favoriteMovies')) || []
+  const movie = movies.find(movie => movie.id === id)
+  if (list.some((movie) => movie.id === id)) {
+    return alert('此電影已經在收藏清單中！')
+  }
+  list.push(movie)
+  localStorage.setItem('favoriteMovies', JSON.stringify(list))
+}
+
+//監聽More功能鍵和收藏(+)功能鍵
+dataPanle.addEventListener('click', function onPanelClicked(event) {
+  if (event.target.matches('.btn-show-movie')) {
+    showMovieModel(Number(event.target.dataset.id))
+  }
+  else if (event.target.matches('.btn-add-favorite')) {
+    addToFavorite(Number(event.target.dataset.id))
+  }
+})
+
+//監聽表單提交事件，submit是搜尋表單的提交事件
+searhForm.addEventListener('submit', function onSearchFormSubmitted(event) {
+  event.preventDefault()
+  //trim()把字串頭尾空格去掉，toLowerCase()把字串轉成小寫
+  const keyword = searchInput.value.trim().toLowerCase()
+
+  filteredMovies = movies.filter((movie) =>
+    movie.title.toLowerCase().includes(keyword)
+  )
+
+  if (filteredMovies.length === 0) {
+    return alert(`您輸入的關鍵字：${keyword} 沒有符合條件的電影`)
+  }
+
+  //用迴圈迭代：for-of
+  //includes字串內包含特定字串內容時，就會得到 true
+  /*for(const movie of movies) {
+    if (movie.title.toLowerCase().includes(keyword)) {
+      filteredMovies.push(keyword)
+    }
+  }*/
+
+  //重製分頁器
+  renderPaginator(filteredMovies.length)
+  //預設顯示第 1 頁的搜尋結果
+  renderMovieList(getMoviesByPage(1))
+
+})
+
+axios
+  .get(INDEX_URL)
+  .then((response) => {
+    /*迭代
+    for (const movie of response.data.results) {
+      movies.push(movie)
+    }*/
+    movies.push(...response.data.results)
+    renderPaginator(movies.length)
+    renderMovieList(getMoviesByPage(1))
+  })
